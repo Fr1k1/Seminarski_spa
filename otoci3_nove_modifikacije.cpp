82b5118b2a2f086916f3f6fa0f8ff2c36b23ee5b
Fr1k1 <martin.friscic6@gmail.com>
2021-12-29 14:05:32 +0100

    slozeno za matrice svih dimenzija unos

--- otoci3_nove_modifikacije.cpp
+++ otoci3_nove_modifikacije.cpp
@@ -2,40 +2,13 @@
 #include "klase_otoci3.h"
 using namespace std;
 
-int numIslands(island<int> &isl, vector<vector<int>> &matrica)
-{
-    int r = matrica.size();
-    int c = matrica[0].size();
-    if (r == 1 && c == 1)
-    {
-        if (matrica[0][0] == '1')
-            return 1;
-        else
-            return 0;
-    }
-
-    int ans = 0;
-    for (int i = 0; i < r; i++)
-    {
-        for (int j = 0; j < c; j++)
-        {
-            if (matrica[i][j] == 1)
-            {
-                ans++;
-                isl.DepthFirstSearch(matrica, i, j, r, c);
-            }
-        }
-    }
-    return ans;
-}
-
 int main()
 {
     island<int> isl;
     vector<vector<int>> matrix;
 
     vector<int> v1;
-    int dimenzija;
+    short retci, stupci;
     char izbor;
     int unos;
 
@@ -51,30 +24,34 @@ int main()
 
         case '1':
 
-            matrix = {{0, 0, 0, 0, 0, 1, 0},
+            /* matrix = {{0, 0, 0, 0, 0, 1, 0},
                       {1, 0, 0, 0, 0, 0, 1},
                       {1, 0, 1, 0, 0, 1, 0},
                       {1, 0, 0, 1, 0, 1, 1},
                       {0, 0, 0, 0, 1, 0, 0},
                       {0, 0, 1, 0, 0, 0, 0},
                       {1, 1, 0, 0, 0, 0, 1}};
+                      */
+            matrix = {{1, 1, 1, 1}, {0, 0, 0, 0}, {1, 1, 0, 0}};
 
-            // cout << "Broj otoka iznosi: " << isl.countIslands(matrix) << endl;
-            cout << "Broj otoka iznosi: " << numIslands(isl, matrix) << endl;
+            cout << "Broj otoka iznosi: " << isl.CountIslands(matrix) << endl;
 
             break;
 
         case '2':
-            cout << "Unesite dimenziju kvadratne matrice\n";
-            cin >> dimenzija;
+            cout << "Unesite broj redaka matrice\n";
+            cin >> retci;
+
+            cout << "Unesite broj stupaca matrice \n";
+            cin >> stupci;
 
             cout << "Unesite matricu\n";
 
-            for (int i = 0; i < dimenzija; i++)
+            for (int i = 0; i < retci; i++)
             {
                 vector<int> v1;
 
-                for (int j = 0; j < dimenzija; j++)
+                for (int j = 0; j < stupci; j++)
                 {
                     cin >> unos;
                     v1.push_back(unos);
@@ -82,7 +59,7 @@ int main()
                 matrix.push_back(v1);
             }
 
-            cout << "Broj otoka iznosi: " << isl.countIslands(matrix) << endl;
+            cout << "Broj otoka iznosi: " << isl.CountIslands(matrix) << endl;
 
             break;
 
